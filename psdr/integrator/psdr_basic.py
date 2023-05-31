f1b25fa509286e8be15d5a72eaa8e6c372e730ce
holmes969 <zhangchengee@gmail.com>
2023-05-30 17:39:01 -0700

    Sync: interior term integrator (WIP)

--- psdr/integrator/psdr_basic.py
+++ psdr/integrator/psdr_basic.py
@@ -19,34 +19,93 @@ class PathSpaceBasicIntegrator(common.PSIntegrator):
                **kwargs # Absorbs unused arguments
     ) -> Tuple[mi.Spectrum, mi.Bool, mi.Spectrum]:
         
-        β = mi.Spectrum(1)              # Path throughput weight
-        L = mi.Spectrum(0)              # Radiance accumulator
-        active = mi.Bool(active)        # Active SIMD lanes
         # Standard BSDF evaluation context for path tracing
         bsdf_ctx = mi.BSDFContext()
-        # Detach ray to avoid differentiable ray intersect
-        si = scene.ray_intersect(dr.detach(ray), active)
-        # Should we continue tracing to reach one more vertex?
-        active_next = si.is_valid()
-        # Get the BSDF. Potentially computes texture-space differentials.
-        bsdf = si.bsdf(ray)
-        # Overwrite wi using path-space formulation
-        wi = dr.normalize(ray.o - si.p)
-        si.wi = dr.select(active_next, si.to_local(wi), si.wi)
-        # path throughput
-        _, cam_imp = sensor.sample_direction(si, mi.Point2f(), active_next)
-        contrb = cam_imp * dr.abs(dr.dot(wi, si.n))
-        contrb = contrb / dr.detach(contrb)
-        β = dr.select(active_next, si.j * contrb, β)
-        # Differentiable evaluation of intersected emitter / envmap
-        Le = si.emitter(scene).eval(si)
-        L += Le * β
-        # emitter sample
-        active_em = active_next & mi.has_flag(bsdf.flags(), mi.BSDFFlags.Smooth)
-        ds, weight_em = scene.sample_emitter_direction(si, sampler.next_2d(), True, active_em)
-        print(ds.j)
-        active_em &= dr.neq(ds.pdf, 0.0)
 
+        # --------------------- Configure loop state ----------------------
+        # Copy input arguments to avoid mutating the caller's state
+        β = mi.Spectrum(1)              # Path throughput weight
+        L = mi.Spectrum(0)              # Radiance accumulator
+        depth = mi.UInt32(0)            # Depth of current vertex
+        active = mi.Bool(active)        # Active SIMD lanes
+
+        # Record the following loop in its entirety
+        loop = mi.Loop(name="Path-Space Diff. Rendering (%s)" % mode.name,
+                       state=lambda: (sampler, ray, depth, L, β, active))
+        while loop(active):
+            # Enable RayFlags.FollowShape to avoid diff. ray-surface intersect
+            si = scene.ray_intersect(ray, mi.RayFlags.All | mi.RayFlags.FollowShape, active)
+            # Overwrite wi using path-space formulation
+            active_next = si.is_valid()
+            wi = dr.normalize(ray.o - si.p)
+            si.wi = dr.select(active_next, si.to_local(wi), si.wi)
+            # Overwrite the primary-ray throughput using path-space formulation
+            is_primary_ray = dr.eq(depth, 0)
+            overwrite_imp = is_primary_ray & active_next
+            ds, cam_imp = sensor.sample_direction(si, mi.Point2f(), overwrite_imp)
+            contrb = cam_imp * dr.abs(dr.dot(wi, si.n))
+            contrb = dr.select(contrb > 0.0, contrb / dr.detach(contrb), 0.0)           # why is this needed?
+            β = β * dr.select(overwrite_imp, si.j * contrb, 1.0)
+            # Direct emission (only for primary ray)
+            Le = si.emitter(scene).eval(si, is_primary_ray)
+            L += Le * β
+
+
+            # # Should we continue tracing to reach one more vertex?
+            # active_next = (depth + 1 < self.max_depth) & active_next
+
+            # # Get the BSDF. Potentially computes texture-space differentials.
+            # bsdf = si.bsdf(ray)
+
+            # # ---------------------- Emitter sampling ----------------------
+            # # Is emitter sampling even possible on the current vertex?
+            # active_em = active_next & mi.has_flag(bsdf.flags(), mi.BSDFFlags.Smooth)
+            # ds, weight_em = scene.sample_emitter_direction(si, sampler.next_2d(), True, active_em)
+            # active_em &= dr.neq(ds.pdf, 0.0)
+            # if dr.any(active_em):
+            #     wo = si.to_local(ds.d)
+            #     bsdf_value_em, bsdf_pdf_em = bsdf.eval_pdf(bsdf_ctx, si, wo, active_em)
+            #     L += bsdf_value_em * weight_em * β
+            
+
+
+            active = active_next
+            depth[active] += 1
+            
+
+
+        #     # path throughput
+        #     ds, cam_imp = sensor.sample_direction(si, mi.Point2f(), active_next)
+        #     contrb = cam_imp * dr.abs(dr.dot(wi, si.n))
+        #     contrb = dr.select(contrb > 0.0, contrb / dr.detach(contrb), 0.0)       # why is this needed?
+        #     β = β * dr.select(active_next, si.j * contrb, 0.0)
+
+        #     # Is emitter sampling even possible on the current vertex?
+        #     active_em = active_next & mi.has_flag(bsdf.flags(), mi.BSDFFlags.Smooth)
+
+        #     active = active_next
+
+
+        # # Get the BSDF. Potentially computes texture-space differentials.
+        # bsdf = si.bsdf(ray)
+        # # Overwrite wi using path-space formulation
+        # wi = dr.normalize(ray.o - si.p)
+        # si.wi = dr.select(active_next, si.to_local(wi), si.wi)
+        # # path throughput
+        # ds, cam_imp = sensor.sample_direction(si, mi.Point2f(), active_next)
+        # contrb = cam_imp * dr.abs(dr.dot(wi, si.n))
+        # contrb = dr.select(contrb > 0.0, contrb / dr.detach(contrb), 0.0)       # why is this needed?
+        # β = β * dr.select(active_next, si.j * contrb, 0.0)
+        # # Differentiable evaluation of intersected emitter / envmap
+        # Le = si.emitter(scene).eval(si)
+        # L += Le * β
+        # # emitter sample
+        # active_em = active_next & mi.has_flag(bsdf.flags(), mi.BSDFFlags.Smooth)
+        # ds, weight_em = scene.sample_emitter_direction(si, sampler.next_2d(), True, active_em)
+        # active_em &= dr.neq(ds.pdf, 0.0)
+        # wo = si.to_local(ds.d)
+        # bsdf_value_em, bsdf_pdf_em = bsdf.eval_pdf(bsdf_ctx, si, wo, active_em)
+        # L += bsdf_value_em * weight_em * β
         return L, active, None
 
 mi.register_integrator("psdr_basic", lambda props: PathSpaceBasicIntegrator(props))
--- psdr/misc/validate.py
+++ psdr/misc/validate.py
@@ -1,9 +1,8 @@
 import sys
-mitsuba_path = "D:\\Cheng\\mitsuba3\\build\\Release\\python"
-psdr_path = "D:\\Cheng\\mitsuba3\\psdr\\integrator"
-
-sys.path.insert(0, mitsuba_path)
-sys.path.insert(0, psdr_path)
+mitsuba_path = "C:\\Users\\holmes969\\mitsuba3\\"
+# mitsuba_path = "D:\\Cheng\\holmes969\\mitsuba3\\"
+sys.path.insert(0, mitsuba_path + "build\\Release\\python")
+sys.path.insert(0, mitsuba_path + "psdr\\integrator")
 
 import mitsuba as mi
 mi.set_variant('cuda_ad_rgb')
@@ -17,20 +16,21 @@ scene = mi.load_file(scene_path, integrator='psdr_basic')
 # Set parameter to be differentiated
 var = mi.Float(0.0)
 dr.enable_grad(var)
-key = 'emitter.vertex_positions'
+key = 'bunny_shape.vertex_positions'
 params = mi.traverse(scene)
 initial_vertex_positions = dr.unravel(mi.Point3f, params[key])
-trafo = mi.Transform4f.translate([var, 0.0, 0.0])    
+trafo = mi.Transform4f.translate([var, 0.0, 0.0])
 params[key] = dr.ravel(trafo @ initial_vertex_positions)
 # Propagate this change to the scene internal state
 params.update()
 # Render and record the computational graph
 image = mi.render(scene, params, spp=512)
 mi.util.write_bitmap("../results/forward.exr", image)
-# exit()
 # Forward-propagate gradients through the computation graph
 dr.forward(var, dr.ADFlag.ClearEdges)
 # Fetch the image gradient values
 grad_image = dr.grad(image)
 # write to files
+grad_image[:, :, 1] = 0.0
+grad_image[:, :, 2] = 0.0
 mi.util.write_bitmap("../results/derivative.exr", grad_image)
